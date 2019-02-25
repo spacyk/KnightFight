616189bffbdece9e70d321a709b179c4ea042e18
Jan Spacek <jan.spacek12@gmail.com>
2019-02-25 18:02:20 +0100

    use list of lists for map

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import json
 
 
 class Element:
-    def __init__(self, name, symbol, position):
+    def __init__(self, name, symbol, row, column):
         self.name = name
-        self.position = position
         self.symbol = symbol
+        self.row = row
+        self.column = column
 
 
 class Knight(Element):
 
-    def __init__(self, name, symbol, position):
-        super().__init__(name, symbol, position)
+    def __init__(self, name, symbol, row, column):
+        super().__init__(name, symbol, row, column)
         self.defence = 1
         self.attack = 1
         self.status = "LIVE"
@@ -31,23 +32,24 @@ class Knight(Element):
         return self_attack > other_defence
 
     def output_repr(self):
-        return [self.position, self.status, self.special_item, self.attack, self.defence]
+        position = None if self.row is None else [self.row, self.column]
+        return [position, self.status, self.special_item, self.attack, self.defence]
 
-    def change_position(self, new_position, position_map):
-        position_map[self.position]['knights'].remove(self)
-        position_map[new_position]['knights'].append(self)
-        self.position = new_position
-
+    def change_position(self, new_row, new_column, position_map):
+        position_map[self.row][self.column]['knights'].remove(self)
+        position_map[new_row][new_column]['knights'].append(self)
+        self.row, self.column = new_row, new_column
         if self.special_item:
-            self.special_item.change_position(new_position)
+            self.special_item.change_position(new_row, new_column)
 
     def set_dead(self, position_map, death_type="DEAD"):
         self.attack, self.defence = 0, 0
         self.status = death_type
-        position_map[self.position]['knights'].remove(self)
-        self.position = None
+        position_map[self.row][self.column]['knights'].remove(self)
+        self.row, self.column = None, None
+        self.special_item = None
         if self.special_item:
-            self.special_item.make_available()
+            self.special_item.make_available(position_map)
 
     def perform_attack(self, other, position_map):
         if self > other:
@@ -60,39 +62,36 @@ class Knight(Element):
         item.assign(position_map)
 
     def get_new_position(self, direction):
-        new_position = ()
         if direction == 'N':
-            row, column = self.position
-            new_position = row - 1, column
-        if direction == 'E':
-            row, column = self.position
-            new_position = row, column + 1
-        if direction == 'S':
-            row, column = self.position
-            new_position = row + 1, column
-        if direction == 'W':
-            row, column = self.position
-            new_position = row, column - 1
-        return new_position
+            row, column = self.row - 1, self.column
+        elif direction == 'E':
+            row, column = self.row, self.column + 1
+        elif direction == 'S':
+            row, column = self.row + 1, self.column
+        elif direction == 'W':
+            row, column = self.row, self.column - 1
+        else:
+            raise ValueError("Invalid direction")
+        return row, column
 
     @staticmethod
-    def get_elements_on_position(position, position_map):
-        position_dict = position_map.get(position)
+    def get_elements_on_position(row, column, position_map):
+        position_dict = position_map[row][column]
         return position_dict['knights'], position_dict['items']
 
     def move(self, direction, position_map):
         if self.status != "LIVE":
-            logging.info("Dead knight can't move anymore")
+            logging.info("No moving for death knight")
             return
 
-        new_position = self.get_new_position(direction)
+        new_row, new_column = self.get_new_position(direction)
 
-        if -1 in new_position or 8 in new_position:
+        if new_row in [-1, 8] or new_column in [-1, 8]:
             self.set_dead(position_map, death_type="DROWNED")
             return
 
-        knights, items = self.get_elements_on_position(new_position, position_map)
-        self.change_position(new_position, position_map)
+        knights, items = self.get_elements_on_position(new_row, new_column, position_map)
+        self.change_position(new_row, new_column, position_map)
 
         if len(knights) == 2:
             self.perform_attack(knights[0], position_map)
@@ -104,8 +103,8 @@ class Knight(Element):
 
 
 class Item(Element):
-    def __init__(self, name, symbol, position, attack, defence, priority):
-        super().__init__(name, symbol, position)
+    def __init__(self, name, symbol, row, column, attack=None, defence=None, priority=None):
+        super().__init__(name, symbol, row, column)
         self.attack = attack
         self.defence = defence
         self.priority = priority
@@ -114,49 +113,53 @@ class Item(Element):
     def __gt__(self, other):
         return self.priority < other.priority
 
-    def change_position(self, new_position):
-        self.position = new_position
+    def change_position(self, new_row, new_column):
+        self.row, self.column = new_row, new_column
 
     def make_available(self, position_map):
-        position_map[self.position]['items'].append(self)
+        self.equipped = False
+        position_map[self.row][self.column]['items'].append(self)
 
     def assign(self, position_map):
         self.equipped = True
-        position_map[self.position].remove(self)
+        position_map[self.row][self.column]['items'].remove(self)
 
     def output_repr(self):
-        return [list(self.position), self.equipped]
+        return [[self.row, self.column], self.equipped]
 
 
 class Game:
     valid_directions = ['N', 'E', 'S', 'W']
 
     def __init__(self):
-        self.axe = Item("axe", "A", (2, 2), 2, 0, 4)
-        self.dagger = Item("dagger", "D", (2, 5), 1, 0, 2)
-        self.helmet = Item("helmet", "H", (5, 5), 0, 1, 1)
-        self.magic_stuff = Item("magic_stuff", "M", (5, 2), 1, 1, 3)
-        self.items = [self.axe, self.dagger, self.helmet, self.magic_stuff]
-
-        self.red = Knight("red", "R", (0, 0))
-        self.blue = Knight("blue", "B", (7, 0))
-        self.green = Knight("green", "G", (7, 7))
-        self.yellow = Knight("yellow", "Y", (0, 7))
+        """Init the game, prepare all the knights and the items and initialize map that keeps track of position of all
+        the elements
+        """
+        self.red = Knight("red", "R", 0, 0)
+        self.blue = Knight("blue", "B", 7, 0)
+        self.green = Knight("green", "G", 7, 7)
+        self.yellow = Knight("yellow", "Y", 0, 7)
         self.knights = {
             self.red.symbol: self.red, self.blue.symbol: self.blue, self.green.symbol: self.green,
             self.yellow.symbol: self.yellow
         }
 
+        self.axe = Item("axe", "A", 2, 2, attack=2, defence=0, priority=4)
+        self.dagger = Item("dagger", "D", 2, 5, attack=1, defence=0, priority=2)
+        self.helmet = Item("helmet", "H", 5, 5, attack=0, defence=1, priority=1)
+        self.magic_stuff = Item("magic_stuff", "M", 5, 2, attack=1, defence=1, priority=3)
+        self.items = [self.axe, self.dagger, self.helmet, self.magic_stuff]
+
         self.position_map = self.init_position_map()
 
     def init_position_map(self):
-        position_map = {(c, r): self.create_map_element() for c in range(8) for r in range(8)}
+        position_map = [[self.create_map_element() for _ in range(8)] for _ in range(8)]
 
         for knight in self.knights.values():
-            position_map[knight.position] = self.create_map_element(knight=knight)
+            position_map[knight.row][knight.column] = self.create_map_element(knight=knight)
 
         for item in self.items:
-            position_map[item.position] = self.create_map_element(item=item)
+            position_map[item.row][item.column] = self.create_map_element(item=item)
         return position_map
 
     @staticmethod
@@ -167,11 +170,11 @@ class Game:
 
     def print_map(self):
         graphic_map = [['_' for _ in range(8)] for _ in range(8)]
-        for element_position, element in self.position_map.items():
-            knights, items = element.get("knights"), element.get("items")
-            row, column = element_position
-            if knights or items:
-                graphic_map[row][column] = knights[0].symbol if knights else items[0].symbol
+        for r, row in enumerate(self.position_map):
+            for c, element in enumerate(row):
+                knights, items = element.get("knights"), element.get("items")
+                if knights or items:
+                    graphic_map[r][c] = knights[0].symbol if knights else items[0].symbol
         for graphic_row in graphic_map:
             print(graphic_row)
         print()
@@ -224,12 +227,13 @@ def testing_execution():
     game = Game()
 
     instructions = list()
+    """
     instructions.append(('R', 'S'))
     instructions.append(('R', 'S'))
     instructions.append(('B', 'E'))
     instructions.append(('G', 'N'))
     instructions.append(('Y', 'N'))
-
+    """
     game.execute_instructions(instructions)
 
     game.print_map()
