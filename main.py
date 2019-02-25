166754f7b8fc5ec26c27387043418cc35e143176
Jan Spacek <jan.spacek12@gmail.com>
2019-02-25 16:14:04 +0100

    working version

--- main.py
+++ main.py
@@ -1,28 +1,68 @@
-class Knight:
+import logging
+from pprint import pprint
+import json
 
-    def __init__(self, name, position):
+
+class Element:
+    def __init__(self, name, symbol, position):
         self.name = name
-        self.defense = 1
-        self.attack = 1
-        self.alive = True
         self.position = position
-        self.special_item = None
+        self.symbol = symbol
 
     def __repr__(self):
-        return f'{self.name}'
+        return f'{self.symbol}'
 
-    def pop_element(self, position, positions_map):
-        return positions_map.pop(position, None)
 
-    def move(self, direction, positions_map):
+class Knight(Element):
 
-        if not self.alive:
-            print("you can't move, ya dead")
-            return
+    def __init__(self, name, symbol, position):
+        super().__init__(name, symbol, position)
+        self.defence = 1
+        self.attack = 1
+        self.status = "LIVE"
+        self.special_item = None
 
-        del positions_map[self.position]
+    def __gt__(self, other):
+        self_attack = 0.5 + self.attack
+        if self.special_item:
+            self_attack += self.special_item.attack
 
-        # switch maybe ak to ma python
+        other_defence = other.defence
+        if other.special_item:
+            other_defence += other.special_item.defence
+
+        return self_attack > other_defence
+
+    def output_repr(self):
+        return [self.position, self.status, self.special_item, self.attack, self.defence]
+
+    def change_position(self, new_position, position_map):
+        position_map[self.position]['knights'].remove(self)
+        position_map[new_position]['knights'].append(self)
+        self.position = new_position
+
+        if self.special_item:
+            self.special_item.change_position(new_position)
+
+    def set_dead(self, position_map, death_type="DEAD"):
+        self.attack, self.defence = 0, 0
+        self.status = death_type
+        position_map[self.position]['knights'].remove(self)
+        self.position = None
+        if self.special_item:
+            self.special_item.make_available()
+
+    def perform_attack(self, other, position_map):
+        if self > other:
+            other.set_dead(position_map)
+        else:
+            self.set_dead(position_map)
+
+    def take_item(self, item, position_map):
+        self.special_item = item
+        item.assign(position_map)
+
+    def get_new_position(self, direction):
         new_position = ()
         if direction == 'N':
             row, column = self.position
@@ -36,74 +76,179 @@ class Knight:
         if direction == 'W':
             row, column = self.position
             new_position = row, column - 1
+        return new_position
+
+    @staticmethod
+    def get_elements_on_position(position, position_map):
+        position_dict = position_map.get(position)
+        return position_dict['knights'], position_dict['items']
+
+    def move(self, direction, position_map):
+        if self.status != "LIVE":
+            logging.info("Dead knight can't move anymore")
+            return
+
+        new_position = self.get_new_position(direction)
 
         if -1 in new_position or 8 in new_position:
-            print('invalid maybe you die')
+            self.set_dead(position_map, death_type="DROWNED")
+            return
 
-        element_on_position = positions_map.pop(self.position, None)
-        if not element_on_position:
-            self.position = new_position
-            positions_map[self.position] = self
+        knights, items = self.get_elements_on_position(new_position, position_map)
+        self.change_position(new_position, position_map)
 
-class Item:
+        if len(knights) == 2:
+            self.perform_attack(knights[0], position_map)
 
-    def __init__(self, name, attack, defence, position, priority):
-        self.name = name
+        if items:
+            if self.status == "LIVE" and not self.special_item:
+                best_item = sorted(items)[0]
+                self.take_item(best_item, position_map)
+
+
+class Item(Element):
+    def __init__(self, name, symbol, position, attack, defence, priority):
+        super().__init__(name, symbol, position)
         self.attack = attack
         self.defence = defence
-        self.position = position
         self.priority = priority
-        self. assigned = False
+        self.equipped = False
 
-    def __repr__(self):
-        return f'{self.name}'
+    def __gt__(self, other):
+        return self.priority < other.priority
+
+    def change_position(self, new_position):
+        self.position = new_position
+
+    def make_available(self, position_map):
+        position_map[self.position]['items'].append(self)
+
+    def assign(self, position_map):
+        self.equipped = True
+        position_map[self.position].remove(self)
+
+    def output_repr(self):
+        return [list(self.position), self.equipped]
 
 
 class Game:
+
+    valid_directions = ['N', 'E', 'S', 'W']
+
     def __init__(self):
+        self.axe = Item("axe", "A", (2, 2), 2, 0, 4)
+        self.dagger = Item("dagger", "D", (2, 5), 1, 0, 2)
+        self.helmet = Item("helmet", "H", (5, 5), 0, 1, 1)
+        self.magic_stuff = Item("magic_stuff", "M", (5, 2), 1, 1, 3)
+        self.items = [self.axe, self.dagger, self.helmet, self.magic_stuff]
 
-        self.axe = Item("A", 2, 0, (2, 2), 4)
-        self.dagger = Item("D", 1, 0, (2, 5), 2)
-        self.helmet = Item("H", 0, 1, (5, 5), 1)
-        self.magicStuff = Item("M", 1, 1, (5, 2), 3)
-
-        self.red = Knight("R", (0, 0))
-        self.blue = Knight("B", (7, 0))
-        self.green = Knight("G", (7, 7))
-        self.yellow = Knight("Y", (0, 7))
-
-        # Bude sa mozno hodit aj opacna mapa ale len na ulahcenie pristupu, ta sa nebude menit
-
-        self.positions_map = {
-            element.position: element for element in
-            [self.red, self.blue, self.green, self.yellow, self.axe, self.dagger, self.helmet, self.magicStuff]
+        self.red = Knight("red", "R", (0, 0))
+        self.blue = Knight("blue", "B", (7, 0))
+        self.green = Knight("green", "G", (7, 7))
+        self.yellow = Knight("yellow", "Y", (0, 7))
+        self.knights = {
+            self.red.symbol: self.red, self.blue.symbol: self.blue, self.green.symbol: self.green,
+            self.yellow.symbol: self.yellow
         }
+
+        self.position_map = self.init_position_map()
+
+    def init_position_map(self):
+        position_map = {(c, r): self.create_map_element() for c in range(8) for r in range(8)}
+
+        for knight in self.knights.values():
+            position_map[knight.position] = self.create_map_element(knight=knight)
+
+        for item in self.items:
+            position_map[item.position] = self.create_map_element(item=item)
+        return position_map
+
+    @staticmethod
+    def create_map_element(item=None, knight=None):
+        items = [item] if item else []
+        knights = [knight] if knight else []
+        return {"knights": knights, "items": items}
 
     def print_map(self):
         graphic_map = [['_' for _ in range(8)] for _ in range(8)]
-        for element_position, element in self.positions_map.items():
+        for element_position, element in self.position_map.items():
+            knights, items = element.get("knights"), element.get("items")
             row, column = element_position
-            graphic_map[row][column] = element.name
+            if knights or items:
+                graphic_map[row][column] = knights[0].symbol if knights else items[0].symbol
         for graphic_row in graphic_map:
             print(graphic_row)
+        print()
 
-def main():
+    def make_output(self):
+        output = {}
+        for knight in self.knights.values():
+            output[knight.name] = knight.output_repr()
+        for item in self.items:
+            output[item.name] = item.output_repr()
+        return output
 
+    def is_valid_instruction(self, instruction):
+        if ':' not in instruction:
+            return False
+        knight, direction = instruction.split(':')
+        if knight not in self.knights.keys():
+            return False
+        if direction not in self.valid_directions:
+            return False
+        return True
+
+    def load_instructions(self):
+        instructions = []
+        with open('moves.txt') as f:
+            f.readline()
+            for line in f:
+                line = line.rstrip('\n')
+                if line == "GAME-END":
+                        break
+                elif self.is_valid_instruction(line):
+                    instructions.append(line.split(':'))
+                else:
+                    raise ValueError("Invalid instruction provided")
+        return instructions
+
+    def save_output(self):
+        with open('final_state.json', 'w') as f:
+            json.dump(self.make_output(), f)
+
+    def perform_move(self, knight_symbol, direction):
+        self.knights[knight_symbol].move(direction, self.position_map)
+
+    def execute_instructions(self, instructions):
+        for instruction in instructions:
+            self.perform_move(*instruction)
+
+
+def testing_execution():
     game = Game()
 
-    print(game.positions_map)
+    instructions = list()
+    instructions.append(('R', 'S'))
+    instructions.append(('R', 'S'))
+    instructions.append(('B', 'E'))
+    instructions.append(('G', 'N'))
+    instructions.append(('Y', 'N'))
+
+    game.execute_instructions(instructions)
+
     game.print_map()
+    pprint(game.make_output())
 
-    game.red.move('S', game.positions_map)
-    game.red.move('S', game.positions_map)
-    game.blue.move('E', game.positions_map)
-    game.green.move('N', game.positions_map)
 
-    print(game.positions_map)
-    game.print_map()
+def main():
+    game = Game()
 
+    instructions = game.load_instructions()
+    game.execute_instructions(instructions)
+
+    game.save_output()
 
 
 if __name__ == "__main__":
-
-    main()
+    #main()
+    testing_execution()
